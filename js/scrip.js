5e4ed6046cdfd389a629030d7a874544ab38e237
eduardomartinez2507 <80860818+eduardomartinez2507@users.noreply.github.com>
2021-05-10 23:57:19 -0400

    Update scrip.js

--- js/scrip.js
+++ js/scrip.js
@@ -14,27 +14,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var doc = document.getElementById('documento').value;
     if (document.querySelector('input[id="rut"]:checked'))
     {
-        var Fn = {
-            // Valida el rut con su cadena completa "XXXXXXXX-X"
-            validaRut : function (rutCompleto) {
-                if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
-                    return false;
-                var tmp 	= rutCompleto.split('-');
-                var digv	= tmp[1]; 
-                var rut 	= tmp[0];
-                if ( digv == 'K' ) digv = 'k' ;
-                return (Fn.dv(rut) == digv );
-            },
-            dv : function(T){
-                var M=0,S=1;
-                for(;T;T=Math.floor(T/10))
-                    S=(S+T%10*(9-M++%6))%11;
-                return S?S-1:'k';
-            }
+        if(doc.length < 9)
+        {
+            alert('Ingrese un rut valido')
+            return;
         }
-        
-        // Uso de la función
-        alert( Fn.validaRut('11111111-1') ? 'Valido' : 'inválido');  
     }
     if (document.querySelector('input[id="pasaporte"]:checked'))
     {
